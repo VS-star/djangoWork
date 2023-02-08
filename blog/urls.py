a4c411eab9eaf4c5e8a6efc41530de354d2ece1a
VS-star <vsstar2310@gmail.com>
2023-02-08 09:25:23 +0100

    add about page and makes blog as main page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='posts'),
+  path('about/', views.about, name='about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # This is function-based view.
 def home(request):
   return HttpResponse('<h1>Blog Home</h1>')
+
+def about(request):
+  return HttpResponse('<h1>This is about page for a Blog</h1>')
