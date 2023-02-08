67ddde1f8b420bd16d4e0af1f1fa881271b9a21a
VS-star <vsstar2310@gmail.com>
2023-02-08 09:19:32 +0100

    Add new route - /blog

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+  'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,8 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-INSTALLED_APPS = [
-  # 'blog.apps.BlogConfig',
-  # or
-  'blog'
-]
+# INSTALLED_APPS = [
+#   'blog.apps.BlogConfig',
+# ]
